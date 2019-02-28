37d319c40444dcafd36a3074f085e0a81521bfa9
alex-tsukanov <aliaksandr.tsukanau@itechart-group.com>
2019-02-28 17:50:32 +0300

    fixed git conflicts

--- src/listening/listener.py
+++ src/listening/listener.py
@@ -3,8 +3,10 @@ from typing import Mapping, Any
 
 from attr import attrs
 
-from handling import Handler
+from handling import Handler, HandlingResult
+from listening import Message
 from parsing.parser import Parser
+from subscribing import Subscriber
 
 
 @attrs(auto_attribs=True, frozen=True)
@@ -25,3 +27,20 @@ class Listener(ABC):
     def on_parsing_failed(self, message: Message, error: Exception):
         raise error
 
+    def on_handled(self, result: HandlingResult):
+        pass
+
+    def _actual_callback(self, message: Message):
+        self.on_received(message)
+        message.ack()
+        try:
+            parsed = self.parser.parse(message)
+            self.on_parsed(original=message, parsed=parsed)
+        except Exception as e:
+            self.on_parsing_failed(message, e)
+            return
+        result = self.handler(parsed)
+        self.on_handled(result)
+
+    def start_listening(self):
+        self.subscriber.subscribe(callback=self._actual_callback)
