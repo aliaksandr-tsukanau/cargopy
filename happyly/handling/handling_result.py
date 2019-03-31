d28a59c843ab05248201369de45791989faf7c05
Semyon Maryasin <simeon@maryasin.name>
2019-04-01 02:08:32 +0300

    More docstrings in handling_result.py

--- happyly/handling/handling_result.py
+++ happyly/handling/handling_result.py
@@ -6,6 +6,10 @@ from .types import ZeroToManyParsedMessages
 
 
 class HandlingResultStatus(Enum):
+    """
+    Handling status: `OK` or `ERR`
+    """
+
     OK = 'OK'
     ERR = 'ERR'
 
@@ -13,12 +17,28 @@ class HandlingResultStatus(Enum):
 @attrs(auto_attribs=True, frozen=True)
 class HandlingResult:
     status: HandlingResultStatus
+    """
+    Status of message handling.
+    """
     data: ZeroToManyParsedMessages
+    """
+    Message or messages which were processed.
+    """
 
     @classmethod
-    def ok(cls, data):
+    def ok(cls, data: ZeroToManyParsedMessages) -> 'HandlingResult':
+        """
+        Construct successful :class:`.HandlingResult`.
+
+        :param data: message or list of messages which were processed.
+        """
         return HandlingResult(status=HandlingResultStatus.OK, data=data)
 
     @classmethod
-    def err(cls, data):
+    def err(cls, data: ZeroToManyParsedMessages) -> 'HandlingResult':
+        """
+        Construct failed :class:`.HandlingResult`.
+
+        :param data: message or list of messages which were processed.
+        """
         return HandlingResult(status=HandlingResultStatus.ERR, data=data)
