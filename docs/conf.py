1a425b0f851ed8ea7c0e8e6a20a7b558980c1f3a
Semyon Maryasin <simeon@maryasin.name>
2019-03-31 20:52:10 +0300

    intersphinx: add marshmallow

--- docs/conf.py
+++ docs/conf.py
@@ -250,7 +250,10 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/3/', None)}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'marshmallow': ('https://marshmallow.readthedocs.io/en/2.x-line/', None),
+}
 
 # -- Options for todo extension ----------------------------------------------
 
