3312e1e3bf08c0713a48e9185b2d2ddb61992e38
Semyon Maryasin <simeon@maryasin.name>
2019-04-04 08:36:33 +0300

    Document StopPipeline exception

--- happyly/exceptions/stop_pipeline.py
+++ happyly/exceptions/stop_pipeline.py
@@ -3,4 +3,9 @@ from attr import attrs
 
 @attrs(auto_attribs=True, auto_exc=True)  # type: ignore
 class StopPipeline(Exception):
+    """
+    This exception should be raised to stop a pipeline.
+    After raising it, :meth:`Executor.on_stopped` will be called.
+    """
+
     reason: str = ''
