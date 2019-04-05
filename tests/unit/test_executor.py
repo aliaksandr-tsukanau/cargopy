7320723f8ffd1c7cda11802acd6f70a49e4ec930
sasha <sasha.ts91@gmail.com>
2019-04-05 12:35:47 +0300

    upd tests

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -1,7 +1,7 @@
 from typing import Any, Mapping
 from unittest.mock import patch
 
-from happyly import Deserializer
+from happyly import Deserializer, Serializer
 from happyly.handling import HandlingResult, HandlingResultStatus
 from happyly.listening import Executor
 from happyly.serialization import DUMMY_SERDE
@@ -68,8 +68,23 @@ def test_executor_no_input(
         on_finished.assert_called_with(original_message=None, error=None)
         on_stopped.assert_not_called()
 
-    assert_callbacks()
+        for callback in [
+            handler,
+            on_stopped,
+            on_finished,
+            on_publishing_failed,
+            on_published,
+            on_serialization_failed,
+            on_serialized,
+            on_handling_failed,
+            on_handled,
+            on_deserialization_failed,
+            on_deserialized,
+            on_received,
+        ]:
+            callback.reset_mock()
 
+    assert_callbacks()
     result = executor.run_for_result()
     assert_callbacks()
     assert result == {'result': 42}
@@ -79,18 +94,26 @@ def test_executor_no_input(
 @patch('test_executor.Executor.on_deserialized')
 @patch('test_executor.Executor.on_deserialization_failed')
 @patch('test_executor.Executor.on_handled')
+@patch('test_executor.Executor.on_handling_failed')
+@patch('test_executor.Executor.on_serialized')
+@patch('test_executor.Executor.on_serialization_failed')
 @patch('test_executor.Executor.on_published')
 @patch('test_executor.Executor.on_publishing_failed')
 @patch('test_executor.Executor.on_finished')
+@patch('test_executor.Executor.on_stopped')
 @patch(
     'test_executor.TestHandler.__call__',
-    return_value=HandlingResult.ok(42),  # type: ignore
+    return_value=HandlingResult.ok({'result': 42}),  # type: ignore
 )
 def test_executor_with_input(
     handler,
+    on_stopped,
     on_finished,
     on_publishing_failed,
     on_published,
+    on_serialization_failed,
+    on_serialized,
+    on_handling_failed,
     on_handled,
     on_deserialization_failed,
     on_deserialized,
@@ -100,22 +123,61 @@ def test_executor_with_input(
         def deserialize(self, message: Any) -> Mapping[str, Any]:
             return {'spam': 'eggs'}
 
+    class TestSer(Serializer):
+        def serialize(self, message_attributes: Mapping[str, Any]) -> Any:
+            return {'i am': 'serialized'}
+
     des = TestDeser()
-    executor = Executor(handler=TestHandler(), deserializer=des, publisher=None)
+    ser = TestSer()
+    executor = Executor(handler=TestHandler(), deserializer=des, publisher=None, serializer=ser)
     assert executor.deserializer == des
+    assert executor.serializer == ser
     assert executor.publisher is None
     executor.run("original message")
-    on_received.assert_called_with("original message")
-    on_deserialized.assert_called_with(
-        original_message="original message", parsed_message={'spam': 'eggs'}
-    )
-    handler.assert_called_with({'spam': 'eggs'})
-    on_handled.assert_called_with(
-        original_message="original message",
-        parsed_message={'spam': 'eggs'},
-        result=HandlingResult(HandlingResultStatus.OK, data=42),
-    )
-    on_published.assert_not_called()
-    on_publishing_failed.assert_not_called()
-    on_deserialization_failed.assert_not_called()
-    on_finished.assert_called_with(original_message="original message", error=None)
+
+    def assert_callbacks():
+        on_received.assert_called_with("original message")
+        on_deserialized.assert_called_with(original_message="original message", parsed_message={'spam': 'eggs'})
+        on_deserialization_failed.assert_not_called()
+        handler.assert_called_with({'spam': 'eggs'})
+        on_handled.assert_called_with(
+            original_message="original message",
+            parsed_message={'spam': 'eggs'},
+            result=HandlingResult(
+                HandlingResultStatus.OK, data={'result': 42}
+            ),  # type: ignore
+        )
+        on_handling_failed.assert_not_called()
+        on_serialized.assert_called_with(
+            original="original message",
+            deserialized={'spam': 'eggs'},
+            result=HandlingResult.ok({'result': 42}),
+            serialized={'i am': 'serialized'},
+        )
+        on_serialization_failed.assert_not_called()
+        on_published.assert_not_called()
+        on_publishing_failed.assert_not_called()
+        on_finished.assert_called_with(original_message='original message', error=None)
+        on_stopped.assert_not_called()
+
+        for callback in [
+            handler,
+            on_stopped,
+            on_finished,
+            on_publishing_failed,
+            on_published,
+            on_serialization_failed,
+            on_serialized,
+            on_handling_failed,
+            on_handled,
+            on_deserialization_failed,
+            on_deserialized,
+            on_received,
+        ]:
+            callback.reset_mock()
+
+    assert_callbacks()
+
+    result = executor.run_for_result("original message")
+    assert_callbacks()
+    assert result == {'i am': 'serialized'}
