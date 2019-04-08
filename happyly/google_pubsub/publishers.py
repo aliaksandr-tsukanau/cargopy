a3a6b0693cb75c5e49e2937900aeb3a414301730
alex-tsukanov <aliaksandr.tsukanau@itechart-group.com>
2019-04-08 13:04:12 +0300

    upd gps publisher

--- happyly/google_pubsub/publishers.py
+++ happyly/google_pubsub/publishers.py
@@ -2,24 +2,22 @@ from typing import Any
 
 from google.cloud import pubsub_v1
 
-from happyly.pubsub import Publisher
+from happyly.pubsub import BasePublisher
 
 
-class GooglePubSubPublisher(Publisher):
-    def __init__(self, project, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.project = project
-        self._publisher_client = pubsub_v1.PublisherClient()
-
-    def __attrs_post_init__(self):
-        self._publisher_client = pubsub_v1.PublisherClient()
-
-    def publish_message(self, serialized_message: Any, to: str):
+class GooglePubSubPublisher(BasePublisher):
+    def publish(self, serialized_message: Any):
         future = self._publisher_client.publish(
-            f'projects/{self.project}/topics/{to}', serialized_message
+            f'projects/{self.project}/topics/{self.to_topic}', serialized_message
         )
         try:
             future.result()
             return
         except Exception as e:
             raise e
+
+    def __init__(self, project: str, to_topic: str):
+        super().__init__()
+        self.project = project
+        self.to_topic = to_topic
+        self._publisher_client = pubsub_v1.PublisherClient()
